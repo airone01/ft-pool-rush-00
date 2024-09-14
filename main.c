b1307dfc98352894a5747a8fbd6ca34fc7750139
cafavier <cannelle.favier@gmail.com>
2024-09-14 13:15:45 +0200

    Ajout main.c norminette ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cafavier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 08:19:40 by cafavier          #+#    #+#             */
-/*   Updated: 2024/09/14 12:52:46 by elagouch         ###   ########.fr       */
+/*   Updated: 2024/09/14 13:14:11 by cafavier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int	main(void)
 	// char	*arg_2;
 	// int	x;
 	// int	y;
-
 	// if (argc < 3)
 	// {
 	// 	write (1, "Usage: rush <x> <y>", 19);
@@ -46,61 +45,49 @@ int	main(void)
 void	pt_corner(int y, int inv_y, int line)
 {
 	if ((inv_y == 0) && (line == 0))
-	{
 		ft_putchar ('A');
-	}
 	else if ((inv_y == 1) && (line == y - 1))
-	{
 		ft_putchar ('A');
-	}
 	else if ((inv_y == 0) && (line == y - 1))
-	{
 		ft_putchar ('C');
-	}
 	else if ((inv_y == 1) && (line == 0))
-	{
 		ft_putchar ('C');
-	}
 }
 
-void	pt_line(int x, int y, int inv_y, int line, int col)
+void	pt_line(int x, int y, int inv_y, int i)
 {
+	int	col;
+	int	inv_z;
+	int	line;
+
+	col = i % x;
+	line = i / x;
 	// Si c'est la PREMIERE OU DERNIERE LIGNE
 	if (line == 0 || line == y - 1)
 	{
 		// Si c'est un coin
 		if (col == 0 || col == x - 1)
-		{
 			pt_corner (y, inv_y, line);
-		}
 		else
-		{
 			ft_putchar ('B');
-		}
 	}
 	else
 	{
 		// Si c'est la PREMIERE OU DERNIERE COLONNE
 		if (col == 0 || col == x - 1)
-		{
 			ft_putchar ('B');
-		}
 		else
-		{
 			ft_putchar (' ');
-		}
 	}
 }
 
 void	rush(int x, int y)
 {
 	int	i;
-	int	line;
 	int	col;
 	int	inv_y;
 
 	inv_y = 0;
-	// Invert if needed
 	if (x < 0)
 		x = -x;
 	if (y < 0)
@@ -112,13 +99,10 @@ void	rush(int x, int y)
 	while (i < (x * y))
 	{
 		col = i % x;
-		line = i / x;
-		pt_line (x, y, inv_y, line, col);
-		// Si c'est LA FIN DE LA LIGNE
+	//	line = i / x;
+		pt_line (x, y, inv_y, i);
 		if (col >= x - 1)
-		{
 			ft_putchar ('\n');
-		}
 		i++;
 	}
 }
